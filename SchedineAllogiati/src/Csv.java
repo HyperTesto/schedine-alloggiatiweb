ec803ce9209adc0ccf38e7afdc02eb2917a318b2
hypertesto <hypertesto@gmail.com>
2014-02-27 19:14:24 +0100

    Aggiunta classe Csv per la creazione del file foramttato come archivio csv

---
+++ SchedineAllogiati/src/Csv.java
@@ -0,0 +1,18 @@
+import org.eclipse.swt.custom.TableTree;
+
+
+public class Csv implements FileGenerator {
+
+	@Override
+	public boolean loadFile(String path) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean writeFile(TableTree records) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
