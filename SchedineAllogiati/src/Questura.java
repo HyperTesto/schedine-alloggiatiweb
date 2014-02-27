a9d05ea312a9c4d2be7e0186d5c1658a6d93f309
hypertesto <hypertesto@gmail.com>
2014-02-27 19:13:18 +0100

    Aggiunta la classe Questura per la generazione del file formattato per la notifica telematica degli alloggiati

---
+++ SchedineAllogiati/src/Questura.java
@@ -0,0 +1,27 @@
+import org.eclipse.swt.custom.TableTree;
+
+/**
+ * @author hypertesto
+ *
+ */
+public class Questura implements FileGenerator {
+
+	/* (non-Javadoc)
+	 * @see FileGenerator#loadFile(java.lang.String)
+	 */
+	@Override
+	public boolean loadFile(String path) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	/* (non-Javadoc)
+	 * @see FileGenerator#writeFile(org.eclipse.swt.custom.TableTree)
+	 */
+	@Override
+	public boolean writeFile(TableTree records) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
