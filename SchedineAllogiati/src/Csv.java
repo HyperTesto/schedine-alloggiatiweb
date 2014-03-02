e5cfadb8fe41c0279362572b33c7ce2d66f8cf04
hypertesto <hypertesto@gmail.com>
2014-03-02 21:46:20 +0100

    Implementato il metodo readFile con tutti i metodi figli (andranno riscritti per una maggiore leggibilità del codice)

--- SchedineAllogiati/src/Csv.java
+++ SchedineAllogiati/src/Csv.java
@@ -1,15 +1,53 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class Csv implements FileManager {
 
 	@Override
-	public boolean loadFile(String path) {
-		// TODO Auto-generated method stub
-		return false;
+	public Record[] loadFile(String path) {
+		
+		Record[] records = null;
+
+		try{	
+			InputStream in = ResourceLoader.loader(path);
+			BufferedReader br = new BufferedReader(new InputStreamReader(in));
+			String strLine;
+			//Read File Line By Line
+			int i=0;
+			while ((strLine = br.readLine()) != null)   {
+				records[i] = readRecord(strLine);
+			}
+			//Close the input stream
+			in.close();
+		}catch (Exception e){//Catch exception if any
+			System.err.println("Error: " + e.getMessage());
+		}
+		return records;
 	}
 
 	@Override
 	public boolean writeFile(Record[] records, String saveTo) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	private void writeRecord(Record record){
+
+	}
+
+	private Record readRecord(String riga){
+		Record record;
+		StringTokenizer t = new StringTokenizer(riga,";");
+		/*
+		 * Istanzio il record leggendo direttamente dalla riga, questa parte sarà da raffianare
+		 * per renderlo più flessibile e meno sensibile ai bug
+		 */
+		record = new Record(t.nextToken(),t.nextToken(),Integer.parseInt(t.nextToken()),t.nextToken(),t.nextToken(),t.nextToken(),t.nextToken(),t.nextToken(),t.nextToken(),t.nextToken(),t.nextToken(),t.nextToken(),t.nextToken());
+		
+		return record;
 	}
 
 }
