9c4763670c9d993146db2748007282d8c9da5ae8
hypertesto <hypertesto@gmail.com>
2014-03-02 17:48:55 +0100

    Aggiunta la classe Permanenza che conterrà le info base sulla tipologia dell'alloggiato e della permanenza

---
+++ SchedineAllogiati/src/Permanenza.java
@@ -0,0 +1,17 @@
+
+public class Permanenza {
+	
+	/*
+	 * Campi prenotazione 
+	 */
+	public String tipoAlloggiato, dataArrivo;
+	public int permanenza;
+	
+	public Permanenza(String tipoAlloggiato, String dataArrivo, int permanenza){
+		this.tipoAlloggiato=tipoAlloggiato;
+		this.dataArrivo=dataArrivo;
+		this.permanenza=permanenza;
+	}
+	
+
+}
