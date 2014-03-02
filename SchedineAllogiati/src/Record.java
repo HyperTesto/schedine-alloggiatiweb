c95f664820fde7968810c23324bee5c9d6b94fe0
hypertesto <hypertesto@gmail.com>
2014-03-02 17:50:14 +0100

    Modificata la classe Record che si occuperà di gesire le classi Permanenza, Alloggiato e Documento (per ora le variabili sono pubbliche per comodità)

--- SchedineAllogiati/src/Record.java
+++ SchedineAllogiati/src/Record.java
@@ -3,5 +3,17 @@
  *
  */
 public class Record {
+	
+	public Alloggiato allog;
+	public Documento doc;
+	public Permanenza perm;
+	
+	public Record(String tipoAlloggiato, String dataArrivo, int permanenza, String nome, String cognome, String dataNascita, String sesso, String cittadinanza, String statoNascita, String comuneNascita, String tipoDoc, String numeroDoc, String rilascioDoc){
+		
+		perm = new Permanenza(tipoAlloggiato, dataArrivo, permanenza);
+		allog = new Alloggiato(nome, cognome, dataNascita, sesso, cittadinanza, statoNascita, comuneNascita);
+		doc = new Documento(tipoDoc, numeroDoc, rilascioDoc);
+	
+	}
 
 }
