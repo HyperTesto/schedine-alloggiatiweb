e2268640a92d8fd14dd5d121916030cfc8007a2a
hypertesto <hypertesto@gmail.com>
2014-03-02 22:28:24 +0100

    Rimossi import non utilizzati

--- SchedineAllogiati/src/Csv.java
+++ SchedineAllogiati/src/Csv.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.InputStream;
 import java.io.InputStreamReader;
