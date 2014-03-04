e2b536be34a8fe074d880b1007fcdad2e7866dea
hypertesto <hypertesto@gmail.com>
2014-03-04 15:26:32 +0100

    Aggiunta javadoc

--- SchedineAllogiati/src/Alloggiato.java
+++ SchedineAllogiati/src/Alloggiato.java
@@ -1,14 +1,26 @@
 /**
  * 
- * @author hypertesto
+ * @author Enrico Testori
  *
  */
 public class Alloggiato {
 	/*
 	 * Dati personali dell'alloggiato
 	 */
+	
+	
 	public String nome, cognome, dataNascita, sesso, cittadinanza, statoNascita, comuneNascita;
 	
+	/**
+	 * Costruttore della classe Alloggiato che contiene dati personali dell'alloggiato stesso.
+	 * @param nome
+	 * @param cognome
+	 * @param dataNascita
+	 * @param sesso
+	 * @param cittadinanza
+	 * @param statoNascita
+	 * @param comuneNascita
+	 */
 	public Alloggiato(String nome, String cognome, String dataNascita, String sesso, String cittadinanza, String statoNascita, String comuneNascita){
 		this.nome=nome;
 		this.cognome=cognome;
