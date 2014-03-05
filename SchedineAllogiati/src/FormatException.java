ea5d17e6ef1a76342d85afcd09abc1cf3b1dbb6d
hypertesto <hypertesto@gmail.com>
2014-03-06 00:12:37 +0100

    Aggiunte variabili statiche per i messaggi di errore.

--- SchedineAllogiati/src/FormatException.java
+++ SchedineAllogiati/src/FormatException.java
@@ -6,12 +6,23 @@
 
 public class FormatException {
 	
+	/*
+	 * Variabili statiche per i messaggi di errore.
+	 */
+	public static String MISSING_CAPO_FAMIGLIA = "Capo famiglia mancante";
+	public static String MISSING_CAPO_GRUPPO = "Capo gruppo mancante";
+	public static String MISSING_MEMBRO_GRUPPO = "Membri gruppo mancanti";
+	public static String MISSING_MEMBRO_FAMIGLIA = "Membri famiglia mancanti";
+	public static String INVALID_RECORD = "Questo record sembra non essere formattato correttamente";
+	public static String PERMANENZA_TOO_LONG = "Il campo permanenza deve essere minore di 30 giorni";
+	public static String REMOTE_DATA_ARRIVO = "La data di arrivo deve essere compresa nelle ultime 48 ore";
+	
 	private Exception exception;
 	private int position;
 	
 	public FormatException(int pos, String tipo){
 		position = pos;
-		exception = new Exception("tipo");
+		exception = new Exception(tipo);
 	}
 	
 	public int getPosition(){
