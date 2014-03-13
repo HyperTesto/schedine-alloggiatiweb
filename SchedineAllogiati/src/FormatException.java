098952b198af7fa03a7c6f8e522ae2c1ecaa8fe7
hypertesto <hypertesto@gmail.com>
2014-03-13 15:05:50 +0100

    Pulizia metodi e varibili superflui

--- SchedineAllogiati/src/FormatException.java
+++ SchedineAllogiati/src/FormatException.java
@@ -6,6 +6,10 @@
 
 public class FormatException extends Exception{
 	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	/*
 	 * Variabili statiche per i messaggi di errore.
 	 */
@@ -17,7 +21,6 @@ public class FormatException extends Exception{
 	public static String PERMANENZA_TOO_LONG = "Il campo permanenza deve essere minore di 30 giorni";
 	public static String REMOTE_DATA_ARRIVO = "La data di arrivo deve essere compresa nelle ultime 48 ore";
 	
-	private Exception exception;
 	private int position;
 	
 	public FormatException(int pos, String tipo){
@@ -31,8 +34,4 @@ public class FormatException extends Exception{
 		return position;
 	}
 	
-	public Exception getException(){
-		return this.getException();
-	}
-
 }
