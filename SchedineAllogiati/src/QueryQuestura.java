69615c94e103dec832d06fcdde01bab5f63fac8e
hypertesto <hypertesto@gmail.com>
2014-03-18 19:01:19 +0100

    Aggiunto metodo connect

--- SchedineAllogiati/src/QueryQuestura.java
+++ SchedineAllogiati/src/QueryQuestura.java
@@ -12,6 +12,24 @@ import java.sql.Statement;
 
 
 public class QueryQuestura {
+	
+	public static void connect() throws SQLException{
+		try {
+			Class.forName("org.sqlite.JDBC");
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Connection connection = null;
+		connection = DriverManager.getConnection("jdbc:sqlite:/home/hypertesto/git/SchedineAlloggiati/SchedineAllogiati/src/res/files/tabelle_questura.db");
+	    Statement statement = connection.createStatement();
+	    statement.setQueryTimeout(30);  // set timeout to 30 sec.
+		
+	}
+	
+	public void disconnect(){
+		
+	}
 
 	public static void main(String[] args) {
 		// load the sqlite-JDBC driver using the current class loader
@@ -30,7 +48,7 @@ public class QueryQuestura {
 	      Statement statement = connection.createStatement();
 	      statement.setQueryTimeout(30);  // set timeout to 30 sec.
 
-	      ResultSet rs = statement.executeQuery("select * FROM codici_luoghi");
+	      ResultSet rs = statement.executeQuery("select * FROM codici_luoghi ORDER BY nome");
 	      while(rs.next())
 	      {
 	        // read the result set
