a38c59302c78132abcfdc1e8a0b86d5f6b71e58b
alberto.bonizzi <aseoista@gmail.com>
2014-03-24 17:26:45 +0100

    Aggiunte eccezioni al metodo getHints ()

--- SchedineAllogiati/src/HintsManager.java
+++ SchedineAllogiati/src/HintsManager.java
@@ -1,18 +1,20 @@
 import java.util.List;
+import java.sql.SQLException;
 
 /**
  * 
  * @author Enrico Testori - Alberto Bonizzi
- *
+ * 
  */
 public interface HintsManager {
 	
 	/**
-	 * Metodo che restituisce una lista di stringhe selezionate
-	 * in base a quando specificato dal parametro typed
+	 * Metodo che restituisce una lista di stringhe selezionate in base a quando
+	 * specificato dal parametro typed
+	 * 
 	 * @param typed
 	 * @return List<String>
 	 */
-	public List<String> getHints(String typed);
-
+	public List<String> getHints (String typed) throws SQLException;
+	
 }
