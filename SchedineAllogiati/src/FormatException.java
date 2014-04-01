dd9b6fd8bd45de35dc0803efd4c167b82d39f119
hypertesto <hypertesto@gmail.com>
2014-04-01 17:03:42 +0200

    Aggiunti messaggi di errore per le ForatException

--- SchedineAllogiati/src/FormatException.java
+++ SchedineAllogiati/src/FormatException.java
@@ -20,6 +20,10 @@ public class FormatException extends Exception{
 	public static String INVALID_RECORD = "Questo record sembra non essere formattato correttamente";
 	public static String PERMANENZA_TOO_LONG = "Il campo permanenza deve essere minore di 30 giorni";
 	public static String REMOTE_DATA_ARRIVO = "La data di arrivo deve essere compresa nelle ultime 48 ore";
+	public static String NULL_RECORD = "Record nullo. Qualcosa è andato storto";
+	public static String INVALID_SEX = "Sesso non valido. Il sesso nel file di tipo questura deve essere o 0 o 1.";
+	public static String MALFORMED_DATE = "Data non formattata correttamente. La data deve essere della forma gg/mm/aaa";
+	
 	
 	private int position;
 	
