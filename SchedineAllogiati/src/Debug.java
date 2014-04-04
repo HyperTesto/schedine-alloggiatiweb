e821748ccfed0dfffb26933c5b9e7bfc127d50f1
hypertesto <hypertesto@gmail.com>
2014-04-04 23:12:54 +0200

    Aggiunta JavaDoc

--- SchedineAllogiati/src/Debug.java
+++ SchedineAllogiati/src/Debug.java
@@ -1,8 +1,20 @@
-
+/**
+ * 
+ * @author Enrico Testori
+ *
+ */
 public class Debug {
 	
-	private static boolean debug = true;
-
+	/**
+	 * true = stampa messaggi di debug
+	 * false = non stampa niente
+	 */
+	private static boolean debug = true;		
+	
+	/**
+	 * Stampa il messaggio passato come parametro con la formattazione per il debug
+	 * @param s
+	 */
 	public static void print (Object s){
 		if(debug)	
 			System.out.println("[DEBUG] " + s);
